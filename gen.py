80c69e2cfedb9f7fa0906c0f4f0f7f512273c686
pixelzerg <pixelzerg@gmail.com>
2020-04-22 15:15:07 +0100

    restructure

--- gen.py
+++ gen.py
@@ -6,27 +6,31 @@ DIR_INPUT = os.path.join(DIR_ROOT, "input")
 DIR_CLIPS = os.path.join(DIR_ROOT, "clips")
 DIR_OUT = os.path.join(DIR_ROOT, "out")
 
+def ffmpeg(command):
+    print(command)
 
 class VideoCutter(ABC):
-    def __init__(self, path):
-        self.path = path
-
-    def cut(self):
+    @abstractmethod
+    def cut(self, path: str):
         pass
 
+class RandomCutter(VideoCutter):
+    def __init__(self, min_length: int = 5, max_length: int = 120):
+        self.min_length = min_length
+        self.max_length = max_length
 
-class ClipJoiner(ABC):
-    def __init__(self, first_video: str, second_video: str):
-        self.first_video = first_video
-        self.second_video = second_video
+    def cut(self, path: str):
+        ffmpeg("wow")
 
+
+class ClipJoiner:
     @abstractmethod
-    def join(self) -> str:
+    def join(self, first_video: str, second_video: str) -> str:
         pass
 
 
 class Generator:
-    def __init__(self, cutter: VideoCutter, joiner: ClipJoiner):
+    def __init__(self, cutter, joiner):
         self.cutter = cutter
         self.joiner = joiner
 
@@ -36,3 +40,4 @@ class Generator:
 
 if __name__ == '__main__':
     os.makedirs(DIR_INPUT, exist_ok=True)
+    Generator(RandomCutter(), ClipJoiner()).gen()
